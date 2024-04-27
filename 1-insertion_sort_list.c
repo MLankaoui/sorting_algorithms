efa310de53b593f15d8c150812a6f0d4d170d533
Fatima20027 <fatima.hailou@icloud.com>
2024-04-27 14:49:58 +0100

    betty fixed fot task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,17 +8,17 @@
 */
 void sw_n(listint_t **head, listint_t **node__on__e, listint_t *node__tw__o)
 {
-    (*node__on__e)->next = node__tw__o->next;
-    if (node__tw__o->next != NULL)
-        node__tw__o->next->prev = *node__on__e;
-    node__tw__o->prev = (*node__on__e)->prev;
-    node__tw__o->next = *node__on__e;
-    if ((*node__on__e)->prev != NULL)
-        (*node__on__e)->prev->next = node__tw__o;
-    else
-        *head = node__tw__o;
-    (*node__on__e)->prev = node__tw__o;
-    *node__on__e = node__tw__o->prev;
+	(*node__on__e)->next = node__tw__o->next;
+	if (node__tw__o->next != NULL)
+		node__tw__o->next->prev = *node__on__e;
+	node__tw__o->prev = (*node__on__e)->prev;
+	node__tw__o->next = *node__on__e;
+	if ((*node__on__e)->prev != NULL)
+		(*node__on__e)->prev->next = node__tw__o;
+	else
+		*head = node__tw__o;
+	(*node__on__e)->prev = node__tw__o;
+	*node__on__e = node__tw__o->prev;
 }
 
 /**
@@ -29,24 +29,24 @@ void sw_n(listint_t **head, listint_t **node__on__e, listint_t *node__tw__o)
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *curr__ent, *inse__rti__ion, *tem__p;
+	listint_t *curr__ent, *inse__rti__ion, *tem__p;
 
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-    curr__ent = (*list)->next;
+	curr__ent = (*list)->next;
 
-    while (curr__ent != NULL)
-    {
-        tem__p = curr__ent->next;
+	while (curr__ent != NULL)
+	{
+		tem__p = curr__ent->next;
 
-        inse__rti__ion = curr__ent->prev;
+		inse__rti__ion = curr__ent->prev;
 
-        while (inse__rti__ion != NULL && curr__ent->n < inse__rti__ion->n)
-        {
-            sw_n(list, &inse__rti__ion, curr__ent);
-            print_list((const listint_t *)*list);
-        }
-        curr__ent = tem__p;
-    }
+		while (inse__rti__ion != NULL && curr__ent->n < inse__rti__ion->n)
+		{
+			sw_n(list, &inse__rti__ion, curr__ent);
+			print_list((const listint_t *)*list);
+		}
+		curr__ent = tem__p;
+	}
 }
