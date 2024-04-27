9f90eb62061d62e674e858c00ec250bd4050696d
Fatima20027 <fatima.hailou@icloud.com>
2024-04-27 14:37:54 +0100

    prototypes were added in sort.h

--- sort.h
+++ sort.h
@@ -25,5 +25,7 @@ void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void sw_n(listint_t **head, listint_t **node__on__e, listint_t *node__tw__o);
 
 #endif
