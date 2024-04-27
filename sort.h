06040df50e3b44e3172ac678f04598423e5a1684
MLankaoui <marouanelankaoui66@gmail.com>
2024-04-27 15:07:20 +0100

    adding neccessary prototypes to sort.h

--- sort.h
+++ sort.h
@@ -7,25 +7,34 @@
 #include <string.h>
 
 /**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
- */
+* struct listint_s - Doubly linked list node
+*
+* @n: Integer stored in the node
+* @prev: Pointer to the previous element of the list
+* @next: Pointer to the next element of the list
+*/
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+
 void bubble_sort(int *array, size_t size);
+
 void selection_sort(int *array, size_t size);
+
 void insertion_sort_list(listint_t **list);
 void sw_n(listint_t **head, listint_t **node__on__e, listint_t *node__tw__o);
+void sww(int *a, int *b);
+
+int lomuto_partition(int *array, int min, int max, size_t size);
+void quick_rec_sorttt(int *array, int min, int max, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif
